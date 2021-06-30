d311ffa49ca65f926904f5700bd8ea1fc9d08584
NethajiChowdary <nethajichowdary555@gmail.com>
2021-06-30 20:55:52 +0530

    Added: Code to Get Largest String from Three Strings

--- src/generics/PrintArray.java
+++ src/generics/PrintArray.java
@@ -4,13 +4,15 @@ public class PrintArray
 {
 	public static void main(String[] args)
 	{	
-		float x =10.5f , y = 27.8f , z = 76.5f;
-		if ( x>y && x>z )
-		System.out.println(x+ " is the largest number");
-		else if ( y>x && y>z)
-		System.out.println(y+  " is the largest number");
-		else
-		System.out.println(z+  " is the largest number");
+	String s1 = "Apple";
+	String s2 = "Banana";
+	String s3 = "Pineapple";
+	if (s1.length() > s2.length() && s1.length() > s3.length())
+		System.out.println("Largest String is "  +s1);
+	else if (s2.length() > s1.length() && s2.length() > s3.length())
+		System.out.println("Largest String is "  +s2);
+	else
+		System.out.println("Largest String is "  +s3);
 	}
 }	
 		
