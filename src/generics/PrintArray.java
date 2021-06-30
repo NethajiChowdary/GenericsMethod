e53ae59ce5fce4a920c5975031255fcfb24653ea
NethajiChowdary <nethajichowdary555@gmail.com>
2021-06-30 21:46:16 +0530

    Added: Code for Generic Method

--- src/generics/PrintArray.java
+++ src/generics/PrintArray.java
@@ -1,18 +1,25 @@
 package generics;
 
-public class PrintArray
+public class PrintArray 
 {
+	public static <T extends Comparable<T>>T maximum(T x, T y, T z)
+	{
+		T max = x;
+		
+		if ( y.compareTo(max) > 0) 
+				max = y;
+		
+		if ( z.compareTo(max) > 0)
+		        max = z;
+		
+		return max;
+	}
 	public static void main(String[] args)
-	{	
-	String s1 = "Apple";
-	String s2 = "Banana";
-	String s3 = "Pineapple";
-	if (s1.length() > s2.length() && s1.length() > s3.length())
-		System.out.println("Largest String is "  +s1);
-	else if (s2.length() > s1.length() && s2.length() > s3.length())
-		System.out.println("Largest String is "  +s2);
-	else
-		System.out.println("Largest String is "  +s3);
+	{
+		System.out.printf("Maximum of %d, %d and %d is %d\n\n", 23,55,64, maximum(23,55,64));
+		System.out.printf("Maximum of %.1f,%.1f and %.1f is %.1f\n\n", 4.5,6.4,77.4, maximum(4.5,6.4,77.4));
+		System.out.printf("Maximum of %s,%s and %s is %s\n", "Mac","Windows","Linux", maximum("Mac","Windows","Linux"));
+		
 	}
 }	
 		
